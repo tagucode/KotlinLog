4b6934c9b7ab033f5e3b5f292bd6d7a80c61017f
tagucode <silverxclock@gmail.com>
2020-05-10 05:38:10 +0900

    if文テスト追加

--- app/src/main/java/jp/techacademy/satoshi/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoshi/kotlinlog/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //ログ出力テスト
         val num1 = 10 + 5 - 2 * 4 / 2
         Log.d("kotlintest","10 + 5 - 2 * 4 / 2 = " + num1)
 
@@ -21,6 +22,23 @@ class MainActivity : AppCompatActivity() {
         val num2 = 10
         val num3 = 20
         Log.d("kotlintest","num2 < num3 = " + (num2 < num3))
+
+        //if分テスト
+        val num = 90
+
+        if (num >=  90) {
+            Log.d("kotlintest","優")
+        } else if (num >= 75) {
+            Log.d("kotlintest","良")
+        } else if (num >= 60) {
+            Log.d("kotlintest","可")
+        } else {
+            Log.d("kotlintest","不可")
+        }
+
+        //when文テスト
+
+
 
     }
 }
