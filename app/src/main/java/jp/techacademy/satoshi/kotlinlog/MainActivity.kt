af1a56b2e0c9892328de3d412944d319acf72333
tagucode <silverxclock@gmail.com>
2020-05-10 23:18:59 +0900

    繰り返し処理追加

--- app/src/main/java/jp/techacademy/satoshi/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoshi/kotlinlog/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.techacademy.satoshi.kotlinlog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log  //追加した行
+import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,8 +38,74 @@ class MainActivity : AppCompatActivity() {
         }
 
         //when文テスト
+        val drink = 2
+
+        when (drink) {
+            0 -> {
+                Log.d("kotlinttest","コーヒーを注文しました")
+            }
+            1 -> Log.d("kotlintest","紅茶を注文しました")
+            2 -> Log.d("kotlintest","ミルクを注文しました")
+            else -> Log.d("kotlintest","オーダーミスです")
+        }
+
+        val drink2 = when (drink) {
+            0 -> "コーヒーを注文しました"
+            1 -> "ミルクを注文しました"
+            2 -> "紅茶を注文しました"
+            else -> "オーダーミスです"
+        }
+        Log.d("kotlintest",drink2)
+
+        val num4 = 60
+        val str = if (num4 >= 90) "優" else "その他"
+        Log.d("kotlintest","" + str)
+
+        //for文テスト
+        for (i in 0 until 10) {
+            Log.d("kotlintest",i.toString())
+        }
+
+        //while文テスト
+        var num5 = 10
+
+        while (num5 < 10) {
+            Log.d("kotlintest","先" + num5.toString())
+            num5++
+        }
+
+        var num6 = 10
+
+        do {
+            Log.d("kotlintest","後" + num6.toString())
+            num6++
+        } while (num6 <10)
 
 
+        for(i in 1 until 6){
+            Log.d("kotlintest","for文の" + i + "回目")
+        }
 
+        var num7 = 1
+
+        while (num7 < 6) {
+            Log.d("kotlintest","while文の" + num7 + "回目")
+            num7++
+        }
+
+        //1～3まで
+        for (i in 1..3){
+            Log.d("kotlintest","..演算子の" + i + "回目")
+        }
+
+        //6から2飛ばしで0まで
+        for (i in 6 downTo 0 step 2){
+            Log.d("kotlintest","downTo関数の" + i + "回目")
+        }
+
+        //0から2飛ばしで6まで
+        for (i in 0 until 6 step 2){
+            Log.d("kotlintest","for文の" + i + "回目")
+        }
     }
 }
